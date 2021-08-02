6574e3a1999f18ac8d7a3f2a2e7b297d272eceec
gordototuma <luisgonzalezm331@gmail.com>
2021-08-02 13:39:15 -0500

    create user

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
-clients = 'Luis,Alejandro,'
+clients = 'Luis, Alejandro, '
 
 def create_client(client_name):
     global clients
-    clients += client_name
-    _add_comma()
+
+    if client_name.lower() is not clients.lower():
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client alredy is in the client\'s list')
+
 
 
 def list_clients():
@@ -14,11 +19,25 @@ def list_clients():
 
 def _add_comma():
     global clients
-    clients +=','
+    clients +=', '
 
+def _print_welcome():
+    print('WELCOME APP COMERCE')
+    print('*'*18)
+    print('What would you like to do today?')
+    print('[C] create client')
+    print('[U] Delete client')
 
 if __name__ == "__main__":
-    list_clients()
-    create_client('John')
-    list_clients()
+    _print_welcome()
+    
+    command = input()
+
+    if command in "cC":
+
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    else:
+        print('Invalid Command')
 
