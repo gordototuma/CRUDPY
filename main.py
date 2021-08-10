ca13011f0c308f300e056096acbcafb8ea7cbaa2
gordototuma <luisgonzalezm331@gmail.com>
2021-08-09 23:59:47 -0500

    change str's clients to lists

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import sys
 
-clients = 'Luis, Alejandro, '
+clients = ['Luis', 'Alejandro']
 
 def create_client(client_name):
     global clients
 
-    if client_name.lower() is not clients.lower():
-        clients += client_name
-        _add_comma()
+    if client_name not in clients:
+        clients.append(client_name)
     else:
         print('Client alredy is in the client\'s list')
 
@@ -14,20 +14,21 @@ def create_client(client_name):
 def update_client(client_name, update_client_name):
     global clients
     if client_name in clients:
-        clients = clients.replace(client_name, update_client_name)
+        clients[clients.index(client_name)] =  update_client_name
     else:
         print('Client is not in clients lists')
 
 
 def list_clients():
     global clients
-    print(clients)
+    for idc, client in enumerate(clients):
+        print(f'{idc}: {client}')
 
 
 def delete_client(client_name):
     global clients
     if  client_name in clients:
-        clients = clients.replace(client_name+",", "")
+        clients.remove(client_name)
     else:
         print('Client is not in clients lists')
 
@@ -35,27 +36,23 @@ def delete_client(client_name):
 def search_client(client_name):
     global clients
 
-
-    for client in clients.split(','):
-        if client != client_name:
+    for client in clients:
+        if client.lower() != client_name.lower():
             continue
         else:
             return True
 
-    """
-    if client_name in clients:
-        return True
-    else:
-        return False
-    """
-
-def _add_comma():
-    global clients
-    clients +=', '
-
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name =  input('What is the client name? ')
+
+        if client_name == 'exit':
+            sys.exit()
+
+    return client_name
 
 
 def _print_welcome():
@@ -92,7 +89,6 @@ if __name__ == "__main__":
     elif command == 'S':
         client_name = _get_client_name()
         found = search_client(client_name)
-        print(found)
         if found:
             print('The client is in the client\'s list')
         else:
